8a36a0aaf51548fb4cf2b410850a44440e6372d1
Mike MacMartin <mmacmartin@gmail.com>
2018-06-03 19:35:50 -0400

    Setting up the initial project, and the base Prism.MVVM shell.

---
+++ RehearsalScheduler/Shell/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Build and launch the Unity bootstrapper that will control the application.
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ RehearsalScheduler/Shell/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Shell.Views;
+
+namespace Shell
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell() => Container.Resolve<MainWindowView>();
+
+        protected override void InitializeShell() => Application.Current.MainWindow.Show();
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType<MainWindowView>();
+        }
+    }
+}
