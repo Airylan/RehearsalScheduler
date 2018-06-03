8a140617c67f6bef05982c2948540957f1d88010
Mike MacMartin <mmacmartin@gmail.com>
2018-06-03 19:52:09 -0400

    Adding in the navigation entries, and getting the basics set up so the UI is relatively easy to start chanigng.

---
+++ RehearsalScheduler/Shell/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Shell.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel([Dependency] IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public string MainContentControlName { get; } = "ContentPanel";
+        public Brush NavigationBackground { get; } = Brushes.AntiqueWhite;
+        public ObservableCollection<ListBoxItem> NavigationEntries { get; }
+            = new ObservableCollection<ListBoxItem>(new List<ListBoxItem>
+            {
+                new ListBoxItem { Tag = "", Content = "" }
+            });
+        public IRegionManager _regionManager { get; }
+
+        private void Navigate(string tag)
+        {
+            _regionManager.RequestNavigate(MainContentControlName, tag);
+        }
+    }
+}
