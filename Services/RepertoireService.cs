fd8a5ec9878464562c1b663a9719650623784045
Mike MacMartin <mmacmartin@gmail.com>
2018-06-03 21:37:33 -0400

    Adding in the first pieces of TDD for building a repertoire of songs.

--- Services/RepertoireService.cs
+++ Services/RepertoireService.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ServiceInterfaces;
+using EntityInterfaces;
+using Microsoft.Practices.Unity;
 
 namespace Services
 {
     public class RepertoireService : IRepertoireService
     {
+        private IRepertoire _Repertoire { get; }
+
+        public RepertoireService([Dependency] IRepertoire repertoire)
+        {
+            _Repertoire = repertoire;
+        }
     }
 }
--- ServicesTests/RepertoireServiceTests.cs
+++ ServicesTests/RepertoireServiceTests.cs
@@ -5,16 +5,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityInterfaces;
+using ServiceInterfaces;
 
 namespace ServicesTests
 {
     [TestClass()]
     public class RepertoireServiceTests
     {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Repertoire = new Moq.Mock<IRepertoire>().Object;
+            RepertoireService = new RepertoireService(Repertoire);
+        }
+        
+        private IRepertoire Repertoire { get; set; }
+        private IRepertoireService RepertoireService { get; set; }
+
         [TestMethod()]
         public void ItShouldAddASong()
         {
-            
+            var song = new Moq.Mock<ISong>();
         }
 
         [TestMethod]
