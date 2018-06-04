29407d1d559a49084a2f78edf9f8fff05be48195
Mike MacMartin <mmacmartin@gmail.com>
2018-06-03 21:29:46 -0400

    Starting to build test framework.

---
+++ ServicesTests/RepertoireServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesTests
+{
+    [TestClass()]
+    public class RepertoireServiceTests
+    {
+        [TestMethod()]
+        public void ItShouldAddASong()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ItShouldRemoveASong()
+        {
+
+        }
+
+        [TestMethod]
+        public void ItShouldRetrieveASong()
+        {
+
+        }
+    }
+}
